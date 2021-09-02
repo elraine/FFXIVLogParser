40b75e86220e3be792e9661d748a34d12f7bb2b6
Elraine <contact@alexischan.me>
2021-09-03 00:04:19 +0200

    feat(parser):damage from line

--- logs.py
+++ logs.py
@@ -6,8 +6,10 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import imageio
 
+# https://github.com/quisquous/cactbot/blob/main/docs/LogGuide.md#ability-damage
+
 #logFolder = "D:\\PikCeLL\\Documents\\TEST"
-logFolder = "D:\\PikCeLL\\Documents\\Logs_FF"
+logFolder = "/home/elraine/Projects/ffxivprogressplotter/sourcelog"
 
 fightID = "80037586"
 phaseColors = ['b','r','y','g']
@@ -18,12 +20,41 @@ wipeRegExp = re.compile(r"33\|([0-9]*)-([0-9]*)-([0-9]*)T([0-9]*):([0-9]*):([0-9
 clearRegExp = re.compile(r"33\|([0-9]*)-([0-9]*)-([0-9]*)T([0-9]*):([0-9]*):([0-9]*).*\|"+fightID+"\|40000003.*")
 startRegExp = re.compile(r"00\|([0-9]*)-([0-9]*)-([0-9]*)T([0-9]*):([0-9]*):([0-9]*).*\|0039\|\|Engage!\|.*")
 
+dmgRegExp = re.compile(r"^(?P<type>(?:2[12]))\|(?P<timestamp>(?:[^|]*))\|(?P<sourceId>(?:[^|]*))\|(?P<source>(?:[^|]*))\|(?P<id>(?:[^|]*))\|(?P<ability>(?:[^|]*))\|(?P<targetId>(?:[^|]*))\|(?P<target>(?:[^|]*))\|(?P<flags>(?:[^|]*))\|(?P<damage>(?:[^|]*))\|(?:[^|]*\|){14}(?P<targetCurrentHp>(?:[^|]*))\|(?P<targetMaxHp>(?:[^|]*))\|(?P<targetCurrentMp>(?:[^|]*))\|(?P<targetMaxMp>(?:[^|]*))\|(?:[^|]*\|){2}(?P<targetX>(?:[^|]*))\|(?P<targetY>(?:[^|]*))\|(?P<targetZ>(?:[^|]*))\|(?P<targetHeading>(?:[^|]*))\|(?P<currentHp>(?:[^|]*))\|(?P<maxHp>(?:[^|]*))\|(?P<currentMp>(?:[^|]*))\|(?P<maxMp>(?:[^|]*))\|(?:[^|]*\|){2}(?P<x>(?:[^|]*))\|(?P<y>(?:[^|]*))\|(?P<z>(?:[^|]*))\|(?P<heading>(?:[^|]*))\|")
+
 minFrameDuration = 0.02
 rampUpTime = 5
 rampDownTime = 2
 gifTime = 15
 
-def parseLog(logFile, dict):
+
+def damageNormalizer(bits):
+	zfilled = bits.zfill(8)
+	# print(f'''{zfilled}-{zfilled[4:6]}''')
+	if zfilled[4:6] == '40':
+		# print('LOTTA damage')
+		# print(f'''{zfilled} zfilled''')
+		# print(f'''{int(zfilled[2:4],base=16)}''')
+		# print(f'''{int(zfilled[6:8],base=16)}''')
+		# print(f'''{hex(int(zfilled[2:4],base=16) - int(zfilled[6:8],base=16))}''')
+
+		# print(f'''{zfilled[6:8]}{zfilled[0:2]}{hex(int(zfilled[2:4],base=16)-int(zfilled[6:8],base=16))[2:]}''')
+		# hex(int(zfilled[2:4],base=16)-int(zfilled[6:8],base=16))
+
+		third = hex(int(zfilled[2:4],base=16) - int(zfilled[6:8],base=16))
+		# print(third)
+		if third[:1] == '-':
+			third = hex(int('FF',base=16) + int(third,base=16))
+		return int(f'''{zfilled[6:8]}{zfilled[0:2]}{third[2:]}''',base=16)
+	else:
+		# print('lol dmg')
+		return int(zfilled[:4],base=16)
+	# return int('424E400F'.zfill(8)[:4],base=16)
+	# return int(bits.zfill(8)[:4],base=16)
+
+
+
+def parseLog(logFile, dict, dmg):
 	with open(logFile, 'r', encoding="utf8") as logSource:
 		startTime = datetime.datetime(9999,12,31)
 		endTime = datetime.datetime(1,1,1)
@@ -42,6 +73,11 @@ def parseLog(logFile, dict):
 					if clearMatch:
 						endTime = datetime.datetime(int(clearMatch.group(1)),int(clearMatch.group(2)),int(clearMatch.group(3)),int(clearMatch.group(4)),int(clearMatch.group(5)),int(clearMatch.group(6)))
 						clear = True
+					else:
+						dmgMatch = dmgRegExp.match(line)
+						if dmgMatch: 
+							print(f'''{dmgMatch.group('source')} - {dmgMatch.group('damage')} - {damageNormalizer(dmgMatch.group('damage'))}''')
+						
 			if endTime > startTime:
 				duration = (endTime-startTime).total_seconds()/60
 				dict[startTime] = (duration,clear)
@@ -49,13 +85,14 @@ def parseLog(logFile, dict):
 				endTime = datetime.datetime(1,1,1)
 
 def parseFolder():
-	dict = SortedDict()
+	timecontrol = SortedDict()
+	dmg = dict()
 	i = 1
 	totalFiles = len(os.listdir(logFolder))
 	for filename in os.listdir(logFolder):
 		print(f"File {i} of {totalFiles}")
 		i += 1
-		parseLog(logFolder+"\\"+filename,dict)
+		parseLog(logFolder+"/"+filename,timecontrol, dmg)
 	plt.xlabel("Pull #")
 	plt.ylabel("Duration (min)")
 	plt.figure(figsize=(16,9))
@@ -114,4 +151,5 @@ def parseFolder():
 		os.remove(filename)
 	plt.show()
 
+# print(damageNormalizer('424E400F'))
 parseFolder()
